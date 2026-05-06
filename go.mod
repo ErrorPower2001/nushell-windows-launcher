70833807d9f4f3fa818b827d8d8cea07d2574932
ErrorPower2001 <ErrorPower2001@outlook.com>
2026-05-06 10:27:26 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module nu_windows_launcher
+
+go 1.26.2
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func main() {
+	userHome, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("无法获取用户目录：%v", err)
+		fmt.Printf("\n")
+		os.Exit(1)
+	}
+
+	nuPath     := flag.String("nushell", "nu.exe", "Nushell 可执行文件的路径")
+	configPath := flag.String("config", filepath.Join(userHome, ".config"), "XDG_CONFIG_HOME 路径")
+	cachePath  := flag.String("cache", filepath.Join(userHome, ".cache"), "XDG_CACHE_HOME 路径")
+	dataPath   := flag.String("data", filepath.Join(userHome, ".local", "share"), "XDG_DATA_HOME 路径")
+
+	flag.Parse()
+
+	extraArgs := flag.Args()
+
+	cmd := exec.Command(*nuPath, extraArgs...)
+
+	cmd.Env = os.Environ()
+	cmd.Env = append(cmd.Env,
+		fmt.Sprintf("XDG_CONFIG_HOME=%s", *configPath),
+		fmt.Sprintf("XDG_CACHE_HOME=%s", *cachePath),
+		fmt.Sprintf("XDG_DATA_HOME=%s", *dataPath),
+	)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		exitErr, ok := err.(*exec.ExitError)
+		if ok {
+			os.Exit(exitErr.ExitCode())
+		}
+		fmt.Printf("Nushell 启动失败：\n")
+		fmt.Printf("%v", err)
+		os.Exit(1)
+	}
+}
