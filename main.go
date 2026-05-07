3784069a1d26b2b8722511513c2abbb793e72083
ErrorPower2001 <ErrorPower2001@outlook.com>
2026-05-07 10:37:27 +0800

    feat: Add 3 var to detect if XDG var was changed

--- main.go
+++ main.go
@@ -23,16 +23,38 @@ func main() {
 
 	flag.Parse()
 
+	/*
+	hideCmd := "\"hide-env XDG_CONFIG_HOME XDG_CACHE_HOME XDG_DATA_HOME\""
+	*/
 	extraArgs := flag.Args()
 
 	cmd := exec.Command(*nuPath, extraArgs...)
 
 	cmd.Env = os.Environ()
-	cmd.Env = append(cmd.Env,
-		fmt.Sprintf("XDG_CONFIG_HOME=%s", *configPath),
-		fmt.Sprintf("XDG_CACHE_HOME=%s", *cachePath),
-		fmt.Sprintf("XDG_DATA_HOME=%s", *dataPath),
-	)
+
+	_, existConfigHome := os.LookupEnv("XDG_CONFIG_HOME")
+	if !existConfigHome {
+		cmd.Env = append(cmd.Env,
+			fmt.Sprintf("XDG_CONFIG_HOME=%s", *configPath),
+			"NUX_INJECTED_CONFIG=1",
+		)
+	}
+
+	_, existCacheHome := os.LookupEnv("XDG_CACHE_HOME")
+	if !existCacheHome {
+		cmd.Env = append(cmd.Env,
+			fmt.Sprintf("XDG_CACHE_HOME=%s", *cachePath),
+			"NUX_INJECTED_CACHE=1",
+		)
+	}
+
+	_, existDataHome := os.LookupEnv("XDG_DATA_HOME")
+	if !existDataHome {
+		cmd.Env = append(cmd.Env,
+			fmt.Sprintf("XDG_DATA_HOME=%s", *dataPath),
+			"NUX_INJECTED_DATA=1",
+		)
+	}
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
